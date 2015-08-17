8a032b7d30b25b3d4d7833db7df93013299011ea
Yaniv Perets <yaniv.perets@gmail.com>
2015-08-17 15:09:35 +0300

    aaaa

--- RepData2.R
+++ RepData2.R
@@ -10,5 +10,6 @@ if(!file.exists("data")) {
         download.file(url=urlDoc1, destfile="data\\repdata-peer2_doc-pd01016005curr.pdf",mode='wb')
         
         urlDoc2<-'https://d396qusza40orc.cloudfront.net/repdata%2Fpeer2_doc%2FNCDC%20Storm%20Events-FAQ%20Page.pdf'
-        download.file(url=urlDoc2, destfile="data\\repdata-peer2_doc-NCDC Storm Events-FAQ Page.pdf",,mode='wb')
+        download.file(url=urlDoc2, destfile="data\\repdata-peer2_doc-NCDC Storm Events-FAQ Page.pdf",mode='wb')
 }
+StromData<-read.csv('data\\repdata-data-StormData.csv')
