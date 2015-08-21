83f6fa411e9127dfd0b1d9fefdb87bb4c4d11859
Yaniv Perets <yaniv.perets@gmail.com>
2015-08-22 00:00:26 +0300

    added

--- RepData2.R
+++ RepData2.R
@@ -29,19 +29,18 @@ str(StromData)
 # look at EVTYPE
 str(StromData$EVTYPE)
 
-# many values(985)and not the expected 48 per the document
+# many values(985) and not the expected 48 per the document.
 # looking at some values
 head(unique(StromData$EVTYPE),50)
 tail(unique(StromData$EVTYPE),50)
 
-library(stringdist)
-library(stringr)
 # By looking at the document section 7, The most left 9 chanrcters after removing blanks are unique.
 # So remove non ascii charcters, remove blanks and look at first 9 letters from the left side of EVTYPE
+library(stringdist)
+library(stringr)
 length(unique(substr(toupper(str_replace_all(StromData$EVTYPE,"[[:punct:][:space:][:digit:]]","")),1,9)))
 
-
-# still got 415 entries
+# Got 415 entries
 # lets check this number per year
 
 #convert the date string to a R date and to a year
@@ -66,34 +65,8 @@ plot(m[,1],m[,2])
 abline(h=48)
 m
 
-# number get close to the 48 from year 2003
-
-
-# Try another approch
-lookup<-c("Astronomical Low Tide", "Avalanche", "Blizzard", 
-          "Coastal Flood", "Cold/Wind Chill", "Debris Flow", 
-          "Dense Fog", "Dense Smoke", "Drought", "Dust Devil", 
-          "Dust Storm", "Excessive Heat", "Extreme Cold/Wind Chill", 
-          "Flash Flood", "Flood", "Freezing Fog", "Frost/Freeze", 
-          "Funnel Cloud", "Hail", "Heat", "Heavy Rain", "Heavy Snow", 
-          "High Surf", "High Wind", "Hurricane (Typhoon)", "Ice Storm", 
-          "Lake-Effect Snow", "Lakeshore Flood", "Lightning", 
-          "Marine Hail", "Marine High Wind", "Marine Strong Wind", 
-          "Marine Thunderstorm Wind", "Rip Current", "Seiche", "Sleet", 
-          "Storm Surge/Tide", "Strong Wind", "Thunderstorm Wind", 
-          "Tornado", "Tropical Depression", "Tropical Storm", "Tsunami", 
-          "Volcanic Ash", "Waterspout", "Wildfire", "Winter Storm", 
-          "Winter Weather")
-lookup1<-str_replace_all(lookup,"[[:punct:][:space:][:digit:]]","")
-lookup2<-unique(toupper(substr(lookup1,1,9)))
-d<-data.frame(l1=lookup,l2=lookup1,l3=lookup2)
-
-a<-unique(StromData$EVTYPE)
-a1<-substr(toupper(str_replace_all(a,"[[:punct:][:space:][:digit:]]","")),1,9)
-r<-amatch(a1,lookup2,maxDist =Inf)
-r<-amatch(a1,lookup2,maxDist =5)
-c<-data.frame(Ev=a,EvP=a1, trans=lookup2[r])
-
+# number get close to the 48 from year 2002
+# lets see later how to try and map them
 
 # look at FATALITIES
 str(StromData$FATALITIES)
@@ -127,7 +100,7 @@ head(StromData$PROPDMGEXP)
 StromData[StromData$PROPDMGEXP %in% c('+','-','?'),c("PROPDMG","year","PROPDMGEXP")]
 # only 6 have non zero  values in 1994 and 1995 only. We will assume muliplier is one for them
 
-# So I'll assume the follwing for the PROPDMGEXP:
+# So I'll assume the follwing multiplier for the PROPDMGEXP:
 # for null value - just take 1 as the mulitplier
 # B - 10 ^ 9
 # m/m - 10 ^ 6
@@ -153,38 +126,79 @@ StromData[StromData$CROPDMGEXP =='?',c("CROPDMG","year")]
 
 # I'll assume the same assuptions as the above for PROPDMGEXP
 
-# add to d.f unified PROPDMGEXP and CROPDMGEXP and callacuate the PROPDMG and CROPDMG
+# add to d.f unified multiplier and callacuate the actual PROPDMG and CROPDMG
 Expdf<-data.frame(
 EXP=c('','-','?','+',0:9,'B','m','M','k','K','h','H'),
 Multplier=c(1,0,0,0,10 ^0,10 ^1,10 ^2,10 ^3,10 ^4,10 ^5,10 ^6,10 ^7,10 ^8,10 ^9,10 ^9,10 ^6,10 ^6,10 ^3,10 ^3,10 ^2,10^2))
 
-StromData<-merge(StromData,Expdf,by.x ='PROPDMGEXP', by.y='EXP',all.x=TRUE)
+StromData<-merge(StromData,Expdf,by.x ='PROPDMGEXP', by.y='EXP')
 library(dplyr)
-
-
 StromData<-mutate(StromData,DMGValue=PROPDMG*Multplier+CROPDMG*Multplier,Population=FATALITIES+INJURIES)
 
+# find the dammges per year in order to check the assignemnt remark
+# "The events in the database start in the year 1950 and end in November 2011. 
+# In the earlier years of the database there are generally fewer events recorded, 
+# most likely due to a lack of good records. 
+# More recent years should be considered more complete."
 
-Totals<-summarise(group_by(StromData,year),totalDMGValue=sum(DMGValue),totalDMGPopulation=sum(Population))
+Totals<-summarise(group_by(StromData,year),totalDMGValue=sum(DMGValue),totalDMGPopulation=sum(Population),count=n())
 
-DMGValue<- select(filter(Totals,totalDMGValue>0),year,totalDMGValue)
-DMGPopulation<- select(filter(Totals,totalDMGPopulation>0),year,totalDMGPopulation)
+DMGValue<- select(filter(Totals,totalDMGValue>0),year,totalDMGValue,count)
+DMGPopulation<- select(filter(Totals,totalDMGPopulation>0),year,totalDMGPopulation,count)
 p<-merge(DMGPopulation,DMGValue)
 library(tidyr)
 library(ggplot2)
 p<-gather(p, DMGtype, val, -year)
 p$year<-as.Date(paste('01/01/',p$year),"%m/%d/%Y")
 
-#par(mfrow=c(2,1))
-#plot(DMGValue$year,DMGValue$totalDMGValue)
-#plot(DMGPopulation$year,DMGPopulation$totalDMGPopulation)
-
 qplot(year, val, data = p, geom = "line", group = DMGtype) +
         facet_grid(DMGtype ~ ., scale = "free_y") 
 
+# Based on the plot we can see that the count of events (with non zero DMG value) is starting to grow
+# after 1990, so before that there are indded not many records.
+# The Population health has data fron the beging
+# The economic samage has mostely data from 1993 only.
+# so based on that we can conclude that from around 1993 we have more complete data
+# so we will continue on data from that period.
+
 StromData1993<-filter(StromData,year>=1993)
 
-Totals1993<-summarise(group_by(StromData,EVTYPE),totalDMGValue=sum(DMGValue),totalDMGPopulation=sum(Population))
+Totals1993<-summarise(group_by(StromData1993,EVTYPE),totalDMGValue=sum(DMGValue),totalDMGPopulation=sum(Population))
 
 DMGValue1993<- filter(Totals1993,totalDMGValue>0)
 DMGPopulation1993<- filter(Totals1993,totalDMGPopulation>0)
+
+
+# No try to match the EVTYPE to the ones defined in the document
+lookup<-c("Astronomical Low Tide", "Avalanche", "Blizzard", 
+          "Coastal Flood", "Cold/Wind Chill", "Debris Flow", 
+          "Dense Fog", "Dense Smoke", "Drought", "Dust Devil", 
+          "Dust Storm", "Excessive Heat", "Extreme Cold/Wind Chill", 
+          "Flash Flood", "Flood", "Freezing Fog", "Frost/Freeze", 
+          "Funnel Cloud", "Hail", "Heat", "Heavy Rain", "Heavy Snow", 
+          "High Surf", "High Wind", "Hurricane (Typhoon)", "Ice Storm", 
+          "Lake-Effect Snow", "Lakeshore Flood", "Lightning", 
+          "Marine Hail", "Marine High Wind", "Marine Strong Wind", 
+          "Marine Thunderstorm Wind", "Rip Current", "Seiche", "Sleet", 
+          "Storm Surge/Tide", "Strong Wind", "Thunderstorm Wind", 
+          "Tornado", "Tropical Depression", "Tropical Storm", "Tsunami", 
+          "Volcanic Ash", "Waterspout", "Wildfire", "Winter Storm", 
+          "Winter Weather")
+
+lookup1<-toupper(str_replace_all(lookup,"[[:punct:][:space:][:digit:]]",""))
+lookup2<-unique(substr(lookup1,1,9))
+d<-data.frame(l1=lookup,l2=lookup1,l3=lookup2)
+
+EVType1933<-unique(c(as.character(unique(DMGPopulation1993$EVTYPE)),as.character(unique(DMGValue1993$EVTYPE))))
+
+#a<-unique(substr(toupper(str_replace_all(EVType1933,"[[:punct:][:space:][:digit:]]","")),1,9))
+a<-unique(toupper(str_replace_all(EVType1933,"[[:punct:][:space:][:digit:]]","")))
+adf<-data.frame(
+        a=a)
+
+a2<-merge(adf,d,by.x='a',by.y='l2',all.x = T)
+a3<-a2[is.na(a2$l1),'a']
+#r<-amatch(a3,lookup1,maxDist =Inf)
+r<-amatch(a3,lookup1,maxDist =1)
+c<-data.frame(EvP=a3, trans=lookup1[r])
+print(as.character(c[is.na(c$trans),"EvP"]),print.gap = 30)
